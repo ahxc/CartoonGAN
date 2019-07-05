c4174db79df2dea204eaa64aacf0cc6cc85eed68
zhangodie <43837296+zhangodie@users.noreply.github.com>
2019-07-05 23:28:10 +0800

    Add files via upload

--- smoother.py
+++ smoother.py
@@ -12,6 +12,7 @@ FLAGS = tf.flags.FLAGS
 flags.DEFINE_string('dataset', 'person2cartoon', 'the path of the training domainB')
 
 flags.DEFINE_integer('img_size', 256, 'image size, default: 256')
+flags.DEFINE_integer('kernel_size', 5, 'kernel size, default: 5')
 #-----------------------------------------------------------------------------------
 # 主进程
 dataset_path = './dataset/'
@@ -24,14 +25,17 @@ check_logdir(trainB_smooth_path)
 
 list_trainB = glob_dataset(trainB_path)
 
+counter, length = 1, len(list_trainB)
+
 for path in list_trainB:
-    img_path = path
-    img = cv2.imread(img_path, 1)
+    img = cv2.imread(path, 1)
     img = cv2.resize(img, (FLAGS.img_size, FLAGS.img_size))
-    img = cv2.blur(img, (3, 3))
+    img = cv2.blur(img, (FLAGS.kernel_size, FLAGS.kernel_size))
 
     filename = path.split('trainB')[-1]
-    trainB_smooth_path = os.path.join(trainB_smooth_path, filename[2:])
+    save_path = os.path.join(trainB_smooth_path, filename[1:])
 
-    cv2.imwrite(trainB_smooth_path, img)
+    cv2.imwrite(save_path, img)
+    print('DONE: %d/%d'%(counter, length))
+    counter += 1
 #-----------------------------------------------------------------------------------
